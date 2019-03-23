506985ae77ebc534a4822f08d5957d9a9b35dbbe
Thorinair <thorinair@yahoo.com>
2019-03-23 20:00:28 +0100

    Added new files.

---
+++ ConfigurationVariPass_EXAMPLE.h
@@ -0,0 +1,11 @@
+/********
+ * VariPass Variables
+ *******/
+
+/* VariPass account key. You can obtain this on the varipass.org website. */
+#define VARIPASS_KEY "aaaaaaaa00000000"
+
+/* VariPass variable IDs. To disable sending of a specific variable, comment it out. */
+#define VARIPASS_ID_TEMPERATURE "aaaa0000"
+#define VARIPASS_ID_HUMIDITY    "aaaa0000"
+#define VARIPASS_ID_BATTERY     "aaaa0000"
